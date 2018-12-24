3b21919b0c315c42630ad060802d6d69990ce975
kvantaris <kvantaris@yandex.ru>
2018-12-24 22:28:59 +0500

    Добавил переход

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour {
+
+    public int numberScene;
+
+	public void RestartLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(numberScene);
+    }
+
+}
